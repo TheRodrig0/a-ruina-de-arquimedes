a67e47bfe4ab3dc633c412c8d52a5f9f38a23098
TheRodrig0 <86856395+TheRodrig0@users.noreply.github.com>
2024-03-01 20:05:23 +0000

    update: the create map function, create more than one layer

--- scripts/scenes.js
+++ scripts/scenes.js
@@ -1,22 +1,28 @@
 // Criar mapa
 let map
 
-function createMap() {
-    let mapData = JSON.parse(game.assets['assets/tilesets/map.json'])
-    let mapData2D = []
+function loadMap() {
+    const mapData = JSON.parse(game.assets['assets/tilesets/map.json'])
+    let mapLayers = mapData.layers
 
+    const mapHeight = mapData.height
 
-    for (let y = 0; y < mapData.height; y++) {
-        let row = []
-        for (let x = 0; x < mapData.width; x++) {
-            let tile = mapData.layers[0].data[y * mapData.width + x]
-            row.push(tile)
+    for (let i = 0; i < mapLayers.length; i++) {
+        let mapTiles = []
+        let arrayData = mapLayers[i].data
+        
+        for (let j = 0; j < arrayData.length; j += mapHeight) {
+            mapTiles.push(arrayData.slice(j, j + mapHeight));
         }
-        mapData2D.push(row)
-    }
 
-    map = new Map(mapData.tilesets[0].tilewidth, mapData.tilesets[0].tileheight);
-    map.image = game.assets['assets/tilesets/Tileset.png'];
-    map.loadData(mapData2D);
-    game.rootScene.addChild(map);
+        let map = new Map(mapData.tilesets[0].tilewidth, mapData.tilesets[0].tileheight)
+        map.image = game.assets['assets/tilesets/Tileset.png']
+        map.loadData(mapTiles.map(subArray => subArray.map(e => e - 1)))
+        stage.addChild(map)
+    }
+}
+
+
+function createMap() {
+    loadMap()
 }
