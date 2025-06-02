70419a9af03266f7149fd36917c8c83fdc388aff
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-06-01 22:17:00 -0300

    feat: implement GameOver scene class extending AbstractScene

---
+++ src/scenes/game-over.ts
@@ -0,0 +1,11 @@
+import { AbstractScene } from "./abstract-scene"
+
+export class GameOver extends AbstractScene {
+    constructor() {
+        super('GameOver')
+    }
+
+    create(): void {
+
+    }
+}
