a9d88b02a6c7d5de86835d619af6124f6f677651
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-06-01 22:17:33 -0300

    refactor: remove unused getMainCameraCenter function from camera position utilities

--- src/utils/camera/position/get-main-center.ts
+++
@@ -1,8 +0,0 @@
-import { Position } from "../../../types/commom/position-interface"
-
-export function getMainCameraCenter(scene: Phaser.Scene): Position {
-    return {
-        x: scene.cameras.main.centerX,
-        y: scene.cameras.main.centerY,
-    }
-}
