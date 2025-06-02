d4a6efd38d90e42cd56ab4ef9bfbff50fc69a69e
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-06-01 22:18:14 -0300

    refactor: update Preloader scene to improve type usage and streamline loading logic

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,8 +1,7 @@
-import { AbstractScene } from "./AbstractScene"
+import { AbstractScene } from "./abstract-scene"
 import { LoadingBar } from "../components/loading-bar"
-import { getMainCameraCenter } from "../utils/camera/position/get-main-center"
 import { goToNextSceneWithFade } from "../utils/scene/transition/go-to-next-scene-with-fade"
-import { Position } from "../types/commom/position-interface"
+import type { Position } from "../types/commom/position-interface"
 
 export class Preloader extends AbstractScene {
     private loadingBar!: LoadingBar
@@ -13,30 +12,29 @@ export class Preloader extends AbstractScene {
     }
 
     init(): void {
-        this.loadingBar = new LoadingBar()
-        this.loadingBar.create(this)
+        this.loadingBar = new LoadingBar(this)
 
-        const MAIN_CAMERA_CENTER: Position = getMainCameraCenter(this)
-        const CURRENT_LOADING_ASSET_TEXT_POSITION: Position = {
-            x: MAIN_CAMERA_CENTER.x,
-            y: MAIN_CAMERA_CENTER.y * 0.9
+        const mainCameraCenter: Position = this.cameraCenter
+        const currentLoadingAssetTextPosition: Position = {
+            x: mainCameraCenter.x,
+            y: mainCameraCenter.y * 0.9
         }
 
         this.add.text(
-            CURRENT_LOADING_ASSET_TEXT_POSITION.x,
-            CURRENT_LOADING_ASSET_TEXT_POSITION.y,
+            currentLoadingAssetTextPosition.x,
+            currentLoadingAssetTextPosition.y,
             "Loading",
         )
             .setOrigin(0.5)
 
-        const CURRENT_ASSET_IN_LOADING_TEXT_POSITION: Position = {
-            x: MAIN_CAMERA_CENTER.x,
-            y: MAIN_CAMERA_CENTER.y * 1.18
+        const currentAssetInLoadingTextPosition: Position = {
+            x: mainCameraCenter.x,
+            y: mainCameraCenter.y * 1.18
         }
 
         this.currentAssetInLoadingText = this.add.text(
-            CURRENT_ASSET_IN_LOADING_TEXT_POSITION.x,
-            CURRENT_ASSET_IN_LOADING_TEXT_POSITION.y,
+            currentAssetInLoadingTextPosition.x,
+            currentAssetInLoadingTextPosition.y,
             "Asset: ",
             {
                 color: '#c1c1c1',
@@ -49,24 +47,26 @@ export class Preloader extends AbstractScene {
     preload(): void {
         this.load.setPath("assets")
         this.load.image("logo", "logos/logo_en.png")
+        this.load.image("logo-phaser", "logos/logo_phaser.png")
+        //this.load.image("background", "backgrounds/background_test.png")
 
         this.load.on("progress", this.loadingBar.updateProgress.bind(this.loadingBar))
         this.load.on('fileprogress', this.updateFilesInLoadingText.bind(this))
         this.load.on("complete", this.completeLoading.bind(this))
     }
 
-    private updateFilesInLoadingText(file: any): void {
+    private updateFilesInLoadingText(file: {key: string}): void {
         this.currentAssetInLoadingText.setText(`Asset: ${file.key}`)
     }
 
     private completeLoading(): void {
-        const DELAY_TIME: number = 2000
+        const delay: number = 2000
 
         goToNextSceneWithFade({
             scene: this,
-            nextSceneKey: "MainMenu",
-            cameraAnimationDuration1: DELAY_TIME / 2,
-            cameraAnimationDuration2: DELAY_TIME / 2
+            nextSceneKey: "Intro",
+            cameraAnimationDuration1: delay / 2,
+            cameraAnimationDuration2: delay / 2
         })
     }
 }
