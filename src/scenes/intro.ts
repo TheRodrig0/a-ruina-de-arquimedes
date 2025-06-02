1372e6f64719dd364b6acefe3476afc1de1220e0
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-06-02 14:33:57 -0300

    refactor: enhance Intro scene by modularizing intro display logic and improving tween application

--- src/scenes/intro.ts
+++ src/scenes/intro.ts
@@ -4,37 +4,91 @@ import { AbstractScene } from "./abstract-scene"
 import { goToNextSceneWithFade } from "../utils/scene/transition/go-to-next-scene-with-fade"
 
 export class Intro extends AbstractScene {
+    private readonly DEFAULT_DELAY = 1500;
+    private readonly TEXT_STYLE = {
+        fontSize: 14,
+        color: "#fff",
+        align: "center"
+    }
+
     constructor() {
         super('Intro')
     }
 
     create() {
-        //const cameraDimensions: Dimensions = this.cameraDimensions
-        //this.add.image(cameraDimensions.width / 2, cameraDimensions.height / 2, //"background")
-        //    .setDisplaySize(cameraDimensions.width, cameraDimensions.height)
-        //    .setAlpha(0.07)
+        const cameraDimensions: Dimensions = this.cameraDimensions
+        this.add.image(cameraDimensions.width / 2, cameraDimensions.height / 2, "background")
+            .setDisplaySize(cameraDimensions.width, cameraDimensions.height)
+        .setAlpha(0.07, 0, 0, 0.07)
 
         const mainCameraCenter: Position = this.cameraCenter
-        const logo = this.add.image(mainCameraCenter.x, mainCameraCenter.y, "logo")
-            .setScale(0.3)
+        const defaultImagesYPosition: number = mainCameraCenter.y * 0.7
 
-        const delay: number = 1500
-        this.applyTweenToObject(logo, delay, 0, (): void => {
-            const logoPhaser = this.add.image(mainCameraCenter.x, mainCameraCenter.y, "logo-phaser")
-                .setScale(0.05)
+        this.showPhaserIntro(mainCameraCenter, defaultImagesYPosition)
 
-            this.applyTweenToObject(logoPhaser, delay, 0, (): void => {
-                goToNextSceneWithFade({
-                    scene: this,
-                    nextSceneKey: "MainMenu",
-                    cameraAnimationDuration1: delay,
-                    cameraAnimationDuration2: delay
-                })
-            })
+    }
+
+    private showPhaserIntro(center: Position, imageY: number): void {
+        const elements = this.createIntroElements(
+            center,
+            imageY,
+            "logo-phaser",
+            0.03,
+            "Powered by Phaser"
+        )
+
+        this.applyTweenToObject(elements, this.DEFAULT_DELAY, 0, () => {
+            this.showGameIntro(center, imageY)
         })
     }
 
-    private applyTweenToObject(target: Phaser.GameObjects.GameObject, duration: number, repeat: number = 0, onComplete?: Phaser.Types.Tweens.TweenOnCompleteCallback) {
+    private showGameIntro(center: Position, imageY: number): void {
+        const elements = this.createIntroElements(
+            center,
+            imageY,
+            "logo",
+            0.25,
+            "A Ruína de Arquimedes"
+        )
+
+        this.applyTweenToObject(elements, this.DEFAULT_DELAY, 0, () => {
+            this.transitionToMainMenu()
+        })
+    }
+
+    private createIntroElements(
+        center: Position,
+        imageY: number,
+        logoKey: string,
+        scale: number,
+        message: string
+    ): Phaser.GameObjects.GameObject[] {
+        const logo = this.add.image(center.x, imageY, logoKey)
+            .setScale(scale)
+
+        const defaultTextYPosition: number = center.y * 1.4
+
+        const text = this.add.text(center.x, defaultTextYPosition, message, this.TEXT_STYLE)
+            .setOrigin(0.5)
+
+        return [logo, text]
+    }
+
+    private transitionToMainMenu(): void {
+        goToNextSceneWithFade({
+            scene: this,
+            nextSceneKey: "MainMenu",
+            cameraAnimationDuration1: this.DEFAULT_DELAY,
+            cameraAnimationDuration2: this.DEFAULT_DELAY
+        })
+    }
+
+    private applyTweenToObject(
+        target: Phaser.GameObjects.GameObject | Phaser.GameObjects.GameObject[],
+        duration: number,
+        repeat: number = 0,
+        onComplete?: Phaser.Types.Tweens.TweenOnCompleteCallback
+    ): void {
         this.tweens.add({
             targets: target,
             alpha: { from: 0, to: 1 },
