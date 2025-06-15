e07ac42d3ae109fc9147bb9ffc30b6188211afa4
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-06-15 17:14:53 -0300

    feat: implement fade-in and fade-out effects in AbstractScene for smoother scene transitions

--- src/scenes/abstract-scene.ts
+++ src/scenes/abstract-scene.ts
@@ -1,9 +1,18 @@
 import type { Position } from "../types/commom/position-interface"
 import type { Dimensions } from "../types/commom/dimensions-interface"
+import { EventManager } from "../core/events/event-manager"
+import { EVENTS } from "../core/events/constants"
+import { fadeIn } from "../utils/effects/object/fade-in"
+import { fadeOut } from "../utils/effects/object/fade-out"
 
 export abstract class AbstractScene extends Phaser.Scene {
+    protected readonly EVENTS = EVENTS
+    protected eventManager: EventManager
+
     constructor(key: string) {
         super(key)
+
+        this.eventManager = EventManager.getInstance(this)
     }
 
     get cameraCenter(): Position {
@@ -19,4 +28,27 @@ export abstract class AbstractScene extends Phaser.Scene {
             height: this.cameras.main.height,
         }
     }
+
+    protected applyFadeInToScene(duration: number = 1000): void {
+        this.cameras.main.setAlpha(0)
+
+        fadeIn({
+            scene: this,
+            target: this.cameras.main,
+            duration
+        })
+    }
+
+    protected startSceneWithFadeOut(nextScene: Phaser.Scene | string, duration: number = 1000): void {
+        fadeOut({
+            scene: this,
+            target: this.cameras.main,
+            duration,
+            onComplete: (): void => {
+                this.scene.start(nextScene)
+            },
+        })
+    }
+
+
 }
